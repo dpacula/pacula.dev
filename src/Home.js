a552f62ddf7e25e38e162358f0754057cd30649d
Derek Pacula <derek@pacula.me>
2020-08-26 15:20:18 -0500

    Creation of minimal home page with flexbox css

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-xs-6">
+          <div class="box"><h1>Hi, I’m Derek. I’m an Full Stack Web Developer from Nebraska. Welcome.</h1></div>
+      </div>
+  </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import './styles/main.scss';
+import './styles/flexboxgrid.min.css';
+import Home from './Home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div class="row">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-10">
+        <Home />
+      </div>
+      <div class="col-lg-2"></div>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
