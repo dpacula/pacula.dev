934b5ad7d4355b808a86bd00f5437b9920510a0d
Derek Pacula <derek@pacula.me>
2020-08-26 17:33:36 -0500

    creation of home page details and overall theme

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import me from '../assets/me.jpg';
 
 function Home() {
   return (
     <div class="row">
-      <div class="col-lg-6 col-md-6 col-xs-6">
-          <div class="box"><h1>Hi, I’m Derek. I’m an Full Stack Web Developer from Nebraska. Welcome.</h1></div>
+      <div class="col-lg-12 col-md-12 col-xs-12">
+          <div class="row">
+            <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 bottom">
+              <div class="main-header"><h1>Hi, I’m Derek. <br></br> I’m an Full Stack Web Developer from Nebraska. Experinced in modern web development.</h1></div>
+            </div>
+            <div class="col-lg-4 top">
+              <div><img class="main-header-img" src={me} alt="me"></img></div>
+            </div>
+          </div>
+      </div>
+      <div class="col-lg-12 col-md-12 col-xs-12">
+          <div class="quick-sell-container">
+            <div class="quick-sell-container-header">
+              <h2>Quick Sell.</h2>
+              <p>When you don't have time to look at everything.</p>
+            </div>
+            <div class="quick-sell-subc">
+              <h3>Experience</h3>
+              <ul>
+                <li>Full Stack Web developer with 5+ years of experince.</li>
+                <li>Co-Lead development of two major start-to-finsh projects utilizing modern web tech including Vue, Angular, Node.JS, and AWS</li>
+                <li>Responsbile for demoing products to clients and boards of clients</li>
+              </ul>
+            </div>
+            <div class="quick-sell-subc">
+              <h3>Projects</h3>
+              <ul>
+                <li>SecureCare Provider Portal</li>
+                <li>Bridge Healthcare (Launching Soon)</li>
+                <li>SerweWX</li>
+              </ul>
+            </div>
+            <div class="quick-sell-subc">
+              <h3>About Me</h3>
+              <ul>
+                <li>Native Nebraskan born and raised.</li>
+                <li>Crazy about tech</li>
+              </ul>
+            </div>
+          </div>
       </div>
   </div>
   );
