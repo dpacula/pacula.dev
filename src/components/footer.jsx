5882eec08e2bbc1e21d3ccaaa7303fb22c1a658a
Derek Pacula <derek@pacula.me>
2020-08-27 12:59:27 -0500

    mobile nav

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 const year = new Date().getFullYear()
 
@@ -13,6 +14,20 @@ function Footer() {
     <div class="copyright">
       <p>Copyright {year} | Derek Pacula</p>
     </div>
+    <nav>
+      <NavLink activeClassName='is-active' to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z"/>
+          <polyline points="5 12 3 12 12 3 21 12 19 12" />
+          <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+          <rect x="10" y="12" width="4" height="4" />
+        </svg>
+        Home
+      </NavLink>
+      <NavLink activeClassName='is-active' to="/about">About</NavLink>
+      <NavLink activeClassName='is-active' to="/projects">Projects</NavLink>
+      <NavLink activeClassName='is-active' to="/resume">Resume</NavLink>
+    </nav>
   </footer>
   );
 }
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -6,7 +6,15 @@ function Navigation() {
   return (
   <header>
     <nav>
-      <NavLink activeClassName='is-active' to="/">Home</NavLink>
+      <NavLink activeClassName='is-active' to="/" exact={true}>
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <polyline points="5 12 3 12 12 3 21 12 19 12" />
+        <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+        <rect x="10" y="12" width="4" height="4" />
+      </svg>
+      Home
+      </NavLink>
       <NavLink activeClassName='is-active' to="/about">About</NavLink>
       <NavLink activeClassName='is-active' to="/projects">Projects</NavLink>
       <NavLink activeClassName='is-active' to="/resume">Resume</NavLink>
