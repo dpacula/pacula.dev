fcceed37fab12728b1e544fb7ab7e11501a49aff
Derek Pacula <derek@pacula.me>
2020-08-27 17:17:55 -0500

    Projects completed

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,10 +2,151 @@ import React from 'react';
 
 function Projects() {
   return (
-    <div class="row">
-      <div class="col-lg-6 col-md-6 col-xs-6">
-          <div class="box"><h1>Projects Page.</h1></div>
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-xs-6">
+        <div class="box">
+          <h2>Professional Projects.</h2>
+          <p>A small sample of some of the work I am involved in.</p>
+        </div>
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="project-box">
+            <h2>SecureCare Provider Portal</h2><label class="year-dev">2018</label>
+            <p>Admin and Provider tool allowing healthcare management, business tools, and other services.</p>
+            <p>Angular, Node.JS, MySQL, AWS</p>
+            <p>
+              <a href="https://portal.securecarecorp.com">Website 
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z"/>
+                  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+                  <line x1="10" y1="14" x2="20" y2="4" />
+                  <polyline points="15 4 20 4 20 9" />
+                </svg>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="project-box">
+            <h2>AutoBridge</h2><label class="year-dev">2020</label>
+            <p>Healthcare tool allowing quick and easy payment processing and allows professionals to view reports.</p>
+            <p>Vue, Node.JS, MySQL, AWS</p>
+            <p>
+              <a href="https://portal.securecarecorp.com">Website 
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z"/>
+                  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+                  <line x1="10" y1="14" x2="20" y2="4" />
+                  <polyline points="15 4 20 4 20 9" />
+                </svg>
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="project-box">
+            <h2>GHX ePay</h2><label class="year-dev">2016</label>
+            <p>The first payment solution to address the financial goals of both healthcare providers and suppliers in one unified platform.</p>
+            <p>.NET, .NET MVC, WebForms, SQL Server, IIS</p>
+            <p>
+              <a href="https://ghx.com/">Website 
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z"/>
+                  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+                  <line x1="10" y1="14" x2="20" y2="4" />
+                  <polyline points="15 4 20 4 20 9" />
+                </svg>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="project-box">
+            <h2>Lester Electrial</h2><label class="year-dev">2018</label>
+            <p>DevOps project to migrate over to AWS automated deployments utilizing ElasticBeanstalk for rapid development.</p>
+            <p>AWS, Angular, Express, Node.JS</p>
+            <p>
+              <a href="https://lesterelectrical.com/applications/">Website 
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z"/>
+                  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+                  <line x1="10" y1="14" x2="20" y2="4" />
+                  <polyline points="15 4 20 4 20 9" />
+                </svg>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-xs-6">
+        <div class="box">
+          <h2>Personal Projects.</h2>
+          <p>A small sample of some of the work I've done on the side.</p>
+        </div>
+    </div>
+  
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="project-box">
+            <h2>pacula.dev</h2>
+            <p>Personal and portfolio website for potential clients and random internet strangers to learn more about me.</p>
+            <p>React, Netlify.</p>
+            <p>GitHub</p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="project-box">
+            <h2>Derek & Matt Wedding</h2><label class="year-dev">2019</label>
+            <p>Wedding website created in Angular, utlizing Apple MapkitJS for maps, and AWS Lambda for guests confirmation.</p>
+            <p>Angular, MapkitJS, AWS</p>
+            <p>GitHub, Website</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <div class="project-box">
+            <h2>NRV Homegrown</h2><label class="year-dev">2019</label>
+            <p>Helped a friend utilize some custom JS to break the limits of Squarespace and third party tools.</p>
+            <p>Javascript ES6</p>
+            <p>
+              <a href="https://www.nrvhomegrown.org">Website 
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z"/>
+                  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+                  <line x1="10" y1="14" x2="20" y2="4" />
+                  <polyline points="15 4 20 4 20 9" />
+                </svg>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+          <div class="project-box">
+            <h2>SerweWX</h2><label class="year-dev">2019</label>
+            <p>Website created to monitor personal weather station. Quick access to poll data from the weather station</p>
+            <p>React, Axios.</p>
+            <p>
+              <a href="https://github.com/">GitHub</a>
+              <a href="https://www.serwewx.com/">Website 
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z"/>
+                  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+                  <line x1="10" y1="14" x2="20" y2="4" />
+                  <polyline points="15 4 20 4 20 9" />
+                </svg>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   );
 }
